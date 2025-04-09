a85f7af6e9fbc96d213d1ad62e6fcc1d29234157
Muhammad Putra Age <muhammadputraage@gmail.com>
2025-04-09 18:42:09 +0800

    update web title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,19 @@ const JetBrainsMono = JetBrains_Mono({
 
 export const metadata: Metadata = {
   icons: "/favicon.ico",
-  title: "Muhammad Putra Age's Portfolio Website",
+  title: "Muhammad Putra Age",
   description:
     "Hi, I'm Muhammad Putra Age, a Frontend Engineer specializing in crafting modern, responsive, and user-friendly web and mobile applications. Explore my portfolio to see my work and services.",
   openGraph: {
-    title: "Muhammad Putra Age's Portfolio Website",
+    title: "Muhammad Putra Age",
     description:
       "Hi, I'm Muhammad Putra Age, a Frontend Engineer specializing in crafting modern, responsive, and user-friendly web and mobile applications. Explore my portfolio to see my work and services.",
     images: ["/assets/og-image.png"],
-    siteName: "Muhammad Putra Age's Portfolio Website",
+    siteName: "Muhammad Putra Age",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Muhammad Putra Age's Portfolio Website",
+    title: "Muhammad Putra Age",
     description:
       "Hi, I'm Muhammad Putra Age, a Frontend Engineer specializing in crafting modern, responsive, and user-friendly web and mobile applications. Explore my portfolio to see my work and services.",
     images: ["/assets/og-image.png"],
